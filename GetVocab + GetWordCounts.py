3a8df78d285830296e62f906f140372c7f25d5c1
AustinSopocko <150801019+AustinSopocko@users.noreply.github.com>
2023-11-28 13:17:53 +0000

    Changed variable names to match specification

--- GetVocab + GetWordCounts.py
+++ GetVocab + GetWordCounts.py
@@ -10,13 +10,13 @@ labels = reviewsFile['label'].values
 def getVocabandFrequencies():
     vec = CountVectorizer(max_features = 10000) #10000 is amount of unique words, this is a random number I chose , will probably change later 
     docTermMatrix = vec.fit_transform(reviews)
-    vocab = vec.get_feature_names_out() #gets list of  all words seen in reviews
+    allWords = vec.get_feature_names_out() #gets list of  all words seen in reviews
     docTermMatrix = docTermMatrix.toarray()
     wordCounts = {} # dictionary for word and their frequency
     for k in range(2):
         wordCounts[k] = defaultdict(lambda: 0) # if word not seen, automatic value in dictionary is 0
     for i in range(docTermMatrix.shape[0]): #iterates through the rows of the 2D array
         currentLabel = labels[i]
-        for j in range(len(vocab)):
-            wordCounts[currentLabel][vocab[j]] += docTermMatrix[i][j] #increments wordcount for word with that label
-    return vocab, wordCounts
+        for j in range(len(allWords)):
+            wordCounts[currentLabel][allWords[j]] += docTermMatrix[i][j] #increments wordcount for word with that label
+    return allWords, wordCounts
