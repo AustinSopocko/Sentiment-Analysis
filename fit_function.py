3784691c6c8db58d8946f025aacb7cea41036e00
AustinSopocko <150801019+AustinSopocko@users.noreply.github.com>
2023-11-29 15:11:58 +0000

    Fit function
    Takes data and returns prior probabilities and number of reviews for each label

---
+++ fit_function.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+
+
+def fit(reviews, labels,labelOptions):
+    reviewTotals = {}
+    conditionalProbabilities = {}
+    groupedReviews = groupDataByLabel(reviews, labels, labelOptions)
+    for label, data in groupedReviews.items():
+        reviewTotals[label] = len(data) #gets number of reviews for that label
+        conditionalProbabilities[label] = math.log(reviewTotals[label]/len(reviews)) #divides number of reviews of that label by total number of reviews.
+    return reviewTotals, conditionalProbabilities
+
+def groupDataByLabel(reviews,labels,labelOptions): #groups reviews by what label it is (0,1)
+    dataDict = {}
+    for label in labelOptions:
+        dataDict[label] = reviews[np.where(label == labels)]
+    return dataDict #this dictionary will have key (0 or 1) and values will be all reviews of negative sentiment and all reviews of positive sentiment
+
