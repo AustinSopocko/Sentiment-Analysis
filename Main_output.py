d1adcf62745febf047a6a0b7cbc8f5f565c1b6df
AustinSopocko <150801019+AustinSopocko@users.noreply.github.com>
2023-12-04 16:17:45 +0000

    added comments to main output file

--- Main_output.py
+++ Main_output.py
@@ -1,14 +1,16 @@
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-labelOptions = [0,1]
+labelOptions = [0,1] # this is an array of all possible sentiment labels
 
-train_reviews, test_reviews, train_labels, test_labels = train_test_split(reviews,labels) # split data
+train_reviews, test_reviews, train_labels, test_labels = train_test_split(reviews,labels) # split data into testing and training
 
-allWords, wordCounts = getVocabandFrequencies()
+allWords, wordCounts = getVocabandFrequencies() #gets set of all words, and dictionary of a word and its corresponding frequency
 
-reviewTotals, conditionalProbabilities = fit(train_reviews,train_labels,labelOptions)
+reviewTotals, conditionalProbabilities = fit(train_reviews,train_labels,labelOptions) #reviewTotals is a dictionary with key : label and value : number of reviews
+#conditional probabilities is a dictionary with key : label and value : log probability of label
 
-prediction = predict(allWords,reviewTotals,wordCounts,labelOptions,test_reviews,conditionalProbabilities)
+prediction = predict(allWords,reviewTotals,wordCounts,labelOptions,test_reviews,conditionalProbabilities) #returns array of new reviews and corresponding predicted label
 
-print("Accuracy on test set - ", accuracy_score(prediction,test_labels))
+print("Accuracy on test set - ", accuracy_score(prediction,test_labels)) #gets accuracy score of model on the data
+
