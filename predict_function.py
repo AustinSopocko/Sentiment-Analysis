0fa1251cec7676f8978bee8d219f6fc558dcb040
AustinSopocko <150801019+AustinSopocko@users.noreply.github.com>
2023-12-04 15:09:23 +0000

    added comments

--- predict_function.py
+++ predict_function.py
@@ -3,17 +3,17 @@ def predict(allWords, reviewTotals,wordCounts,labelOptions,reviews,conditionalPr
     labelScores = {}
     result = []
 
-    for review in reviews:
+    for review in reviews:  #for each review
         labelScores = {currentLabel: conditionalProbabilities[currentLabel] for currentLabel in labelOptions}
         #turns review into set of words
         tokenizer = nltk.tokenize.WhitespaceTokenizer()
         wordSet = tokenizer(review)
-        for word in wordSet:
+        for word in wordSet: #loops through words in review
             if word in allWords: #if we have already seen this word in training phase
-                for label in labelOptions: #looping through each label
-                    laplaceScore = laplace(wordCounts,allWords,word,reviewTotals,label)
-                    labelScores[label] += laplaceScore
-        result.append(max(labelScores, key=labelScores.get))
+                for label in labelOptions: #for each sentiment (0,1)
+                    laplaceScore = laplace(wordCounts,allWords,word,reviewTotals,label) #gets laplace score for that word
+                    labelScores[label] += laplaceScore #adds score for label
+        result.append(max(labelScores, key=labelScores.get)) #adds label with the highest score for that review to result array  
 
     return result
 
